b257167eaf5519a46dca4ec78bf3b1728de0a2c1
Elodie-st <elodie.somerlinck@student.heaj.be>
2025-06-06 11:28:06 +0200

    page credit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -19,35 +19,4 @@ function toggleVideos() {
   window.addEventListener("resize", toggleVideos);
 
 
-  gsap.registerPlugin(ScrollTrigger);
 
-  gsap.to("#sun", {
-    y: 200, // déplacement vers le bas
-    ease: "none",
-    scrollTrigger: {
-      trigger: "body",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: 1 // effet de suivi fluide
-    }
-  });
-/*
-  gsap.registerPlugin(ScrollTrigger);
-
-  gsap.to("#sun", {
-    y: () => {
-      const sun = document.querySelector("#sun");
-      const hand = document.querySelector("#hand");
-      const sunTop = sun.getBoundingClientRect().top;
-      const handTop = hand.getBoundingClientRect().top;
-      return handTop - sunTop - 50; // ajuste le -50 si besoin
-    },
-    ease: "none",
-    scrollTrigger: {
-      trigger: "body", // 👈 plus besoin de scroll-wrapper !
-      start: "top top",
-      endTrigger: "#hand",
-      end: "top center",
-      scrub: true
-    }
-  });*/
